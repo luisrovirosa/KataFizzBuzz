4c051eb4bec3f18888eaf7061f3e5b879be59e24
Luis Rovirosa <luisrovirosa@gmail.com>
2014-03-29 19:56:12 +0100

    Refáctoring: Todos los múltiplos de 5 son Buzz

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -7,12 +7,12 @@ FizzBuzz.prototype.getNumbers = function() {
   for (var i = 1; i < 100; i++) {
     if (i % 3 === 0) {
       numbers.push('Fizz');
+    } else if (i % 5 === 0) {
+      numbers.push('Buzz');
     } else {
       numbers.push(i);
     }
   }
-  numbers[4] = 'Buzz';
-  numbers[9] = 'Buzz';
   return numbers;
 };
 
