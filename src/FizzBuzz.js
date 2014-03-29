6fac61c6c2edb3815cdaa0dc5a45a4afa14ccdb1
Luis Rovirosa <luisrovirosa@gmail.com>
2014-03-29 21:04:08 +0100

    Refactoring: Mejorada la gestión de las reglas

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,27 +1,38 @@
 function FizzBuzz() {
-
+  this.rules = [
+    {
+      method: isFizzBuzz,
+      value: 'FizzBuzz'
+    },
+    {
+      method: isFizz,
+      value: 'Fizz'
+    },
+    {
+      method: isBuzz,
+      value: 'Buzz'
+    }
+  ];
 }
 
 FizzBuzz.prototype.getNumbers = function() {
   var numbers = [];
   for (var i = 1; i < 100; i++) {
-    numbers.push(calculate(i));
+    numbers.push(this.calculate(i));
   }
 
   return numbers;
 };
 
-var calculate = function(number) {
-  var response = number;
-  if (isFizzBuzz(number)) {
-    response = 'FizzBuzz';
-  } else if (isFizz(number)) {
-    response = 'Fizz';
-  } else if (isBuzz(number)) {
-    response = 'Buzz';
+FizzBuzz.prototype.calculate = function(number) {
+  for (var i = 0; i < this.rules.length; i++) {
+    var rule = this.rules[i];
+    var method = rule['method'];
+    if (method(number)) {
+      return rule['value'];
+    }
   }
-
-  return response;
+  return number;
 };
 
 var isFizzBuzz = function(number) {
